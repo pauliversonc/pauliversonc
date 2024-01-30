2c6405e0377ade7f521b3e52de3909a0e9e2c462
Paul Iverson Cortez <pauliversonc@gmail.com>
2024-01-30 11:39:15 +0800

    refactor footer

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -1,5 +1,63 @@
 <template >
+  <footer class="px-4 text-center  xs:px-6 md:px-8 lg:px-10 xl:px-12 mt-[8rem]  2xl:px-16 py-8 container mx-auto ">
 
+<!-- logo -->
+<a href="#" class="inline-block" >
+  <img
+    class="h-12 hidden dark:block"
+    src="/images/logo/icon-white.png"
+    alt="Paul Iverson Cortez - PIC Logo"
+  />
+
+  <img
+    class="h-12 block dark:hidden"
+    src="/images/logo/icon-black.png"
+    alt="Paul Iverson Cortez - PIC Logo"
+  />
+</a>
+
+<div class="text-center dark:text-dmWhite">Paul Iverson Cortez | Portfolio</div>
+
+<!-- credits -->
+<div class="flex flex-col items-center">
+  <span class="font-secondary font-medium">&copy; 2014 &#x2022; Paul Iverson Cortez</span>
+  <span class=" text-gray-500 dark:text-dmBlackTint2 font-medium text-sm">All RIGHTS RESERVED</span>
+</div>
+
+<!-- THANK YOU -->
+<section class="text-gray-900 dark:text-dmWhite mt-[2rem]  container mx-auto">
+<h3 class="uppercase leading-none font-medium  flex items-center justify-center
+text-[2.6rem]
+xxs:text-[3.4rem]
+xs:text-[4.4rem]
+sm:text-[5.8rem]
+md:text-[6.9rem]
+lg:text-[9.4rem]
+xl:text-[11.8rem]
+2xl:text-[14.15rem]
+gap-4
+relative
+">
+<span class=" bg-clip-text text-transparent bg-gradient-to-b from-gray-100 to-white dark:from-dmGray dark:to-dmBlack
+     absolute top-0 left-0 text-gray-200 z-[-1] translate-y-[-50%] font-normal inline-block p-1
+     text-[2.4rem]
+     xxs:text-[3.4rem]
+     xs:text-[4.4rem] 
+     sm:text-[4.9rem]
+     lg:text-[6.1rem] 
+     xl:text-[7.5rem] 
+     md:left-[2%]
+
+    
+    ">/05</span>
+  <span class=" ">salamat</span>
+  <span class="italic">po</span>
+</h3>
+</section>
+
+
+
+</footer>
 </template>
 <script>
 export default {
