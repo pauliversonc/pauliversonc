7f57901557564aa08298e2a2fb720944f5d3d84e
Paul Iverson Cortez <pauliversonc@gmail.com>
2024-01-30 10:53:11 +0800

    refactor theintro

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,12 @@
   <TheHero></TheHero>
   <!-- SCROLL BUTTON -->
   <TheScroll></TheScroll>
-
-
-
-
   <!-- BRIEF INTRO -->
-  <section class="px-4 xs:px-6 md:px-8 lg:px-10 xl:px-12  2xl:px-16 py-8 container mx-auto sm:grid sm:grid-cols-2 sm:gap-4 sm:items-center">
-    <h2 class=" tracking-tighter text-gray-400 dark:text-dmGray container mx-auto
-    text-4xl
-    xs:text-[2.6rem]
-    
-    ">
-      I create <span class="text-gray-900 dark:text-dmWhite">innovative solutions</span> for the evolving <span class="text-gray-900 dark:text-dmWhite">digital landscape</span>
-    </h2>
-    <div class=" hidden sm:block">
-      <img class="block dark:hidden" src="../public/svg/undraw_progressive_app_m-9-ms-dark.svg" alt="filler undraw" >
-      <img class="hidden dark:block" src="../public/svg/undraw_progressive_app_m-9-ms-light.svg" alt="filler undraw" >
-    </div>
-  </section>
+  <TheIntro></TheIntro>
+
+
+
+
 
   <!-- HEADING PER SECTION (ABOUT) -->
   <section class="px-4 xs:px-6 md:px-8 lg:px-10 xl:px-12  2xl:px-16 text-gray-900 dark:text-dmWhite mb-[2rem] mt-[8rem] container mx-auto">
@@ -1160,6 +1148,7 @@ import { useDark } from '@vueuse/core'
 import TheHeader from './components/layout/TheHeader.vue';
 import TheHero from './components/layout/TheHero.vue';
 import TheScroll from './components/layout/TheScroll.vue';
+import TheIntro from './components/layout/TheIntro.vue';
 
 export default {
   name: "PauliversoncApp",
@@ -1167,6 +1156,7 @@ export default {
     TheHeader,
     TheHero,
     TheScroll,
+    TheIntro,
   },
   
   created() {
---
+++ src/components/layout/TheIntro.vue
@@ -0,0 +1,23 @@
+<template >
+  <section class="px-4 xs:px-6 md:px-8 lg:px-10 xl:px-12  2xl:px-16 py-8 container mx-auto sm:grid sm:grid-cols-2 sm:gap-4 sm:items-center">
+    <h2 class=" tracking-tighter text-gray-400 dark:text-dmGray container mx-auto
+    text-4xl
+    xs:text-[2.6rem]
+    
+    ">
+      I create <span class="text-gray-900 dark:text-dmWhite">innovative solutions</span> for the evolving <span class="text-gray-900 dark:text-dmWhite">digital landscape</span>
+    </h2>
+    <div class=" hidden sm:block">
+      <img class="block dark:hidden" src="/svg/undraw_progressive_app_m-9-ms-dark.svg" alt="filler undraw" >
+      <img class="hidden dark:block" src="/svg/undraw_progressive_app_m-9-ms-light.svg" alt="filler undraw" >
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'PauliversoncTheIntro',
+}
+</script>
+<style scoped>
+  
+</style>
