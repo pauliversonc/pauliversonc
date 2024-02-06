2cdecb13b15d2be464084188cbd2f3b9a2406ee2
Paul Iverson Cortez <pauliversonc@gmail.com>
2024-02-06 12:40:10 +0800

    add pre-spacer

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,89 +1,99 @@
 <template>
   <!-- HEADER -->
-  <header ref="header" class="container h-[64px]  mx-auto py-2 px-4 xs:px-6 md:px-8 lg:px-10 xl:px-12  2xl:px-16 flex items-center justify-between relative">
-    <!-- ICON -->
-    <a href="#" >
-      <img
-        class="h-10 hidden dark:block"
-        src="/images/logo/icon-white.png"
-        alt="Paul Iverson Cortez - PIC Logo"
-      />
+  <div class="fixed translate-x-[-50%] top-0 bg-white dark:bg-dmBlack w-full left-[50%] z-[9999]">
+    <header ref="header" class="container h-[64px] mx-auto py-2 px-4 xs:px-6 md:px-8 lg:px-10 xl:px-12  2xl:px-16 flex items-center justify-between relative">
+      <!-- ICON -->
+      <a href="#" >
+        <img
+          class="h-10 hidden dark:block"
+          src="/images/logo/icon-white.png"
+          alt="Paul Iverson Cortez - PIC Logo"
+        />
 
-      <img
-        class="h-10 block dark:hidden"
-        src="/images/logo/icon-black.png"
-        alt="Paul Iverson Cortez - PIC Logo"
-      />
-    </a>
+        <img
+          class="h-10 block dark:hidden"
+          src="/images/logo/icon-black.png"
+          alt="Paul Iverson Cortez - PIC Logo"
+        />
+      </a>
 
-    <!-- wrapper -->
-    <div class="flex gap-4">
+      <!-- wrapper -->
+      <div class="flex gap-4">
 
-      <!-- TOGGLE DARKMODE BUTTON -->
-     <BaseToggleSwitch :btn-min="true"></BaseToggleSwitch>
+        <!-- TOGGLE DARKMODE BUTTON -->
+      <BaseToggleSwitch :btn-min="true"></BaseToggleSwitch>
 
-      <!-- BUTTON MENU -->
-      <div @click="toggleNav" class="relative h-6 w-6 overflow-hidden  z-40 hover:cursor-pointer select-none
-      lg:hidden" role="button" >
-        <div :class="moveLineMenu1" class="ease-out duration-300 absolute h-[4px] w-6 dark:bg-dmWhite bg-gray-900 top-0 ">&nbsp;</div>
-        <div :class="moveLineMenu2" class="ease-out duration-300 absolute h-[4px] w-6 dark:bg-dmWhite bg-gray-900 top-1/2 left-1/2 translate-y-[-50%]">&nbsp;</div>
-        <div :class="moveLineMenu3" class="ease-out duration-300 absolute h-[4px] w-6 dark:bg-dmWhite bg-gray-900 bottom-0">&nbsp;</div>
+        <!-- BUTTON MENU -->
+        <div @click="toggleNav" class="relative h-6 w-6 overflow-hidden  z-40 hover:cursor-pointer select-none
+        lg:hidden" role="button" >
+          <div :class="moveLineMenu1" class="ease-out duration-300 absolute h-[4px] w-6 dark:bg-dmWhite bg-gray-900 top-0 ">&nbsp;</div>
+          <div :class="moveLineMenu2" class="ease-out duration-300 absolute h-[4px] w-6 dark:bg-dmWhite bg-gray-900 top-1/2 left-1/2 translate-y-[-50%]">&nbsp;</div>
+          <div :class="moveLineMenu3" class="ease-out duration-300 absolute h-[4px] w-6 dark:bg-dmWhite bg-gray-900 bottom-0">&nbsp;</div>
+        </div>
+
+
       </div>
 
+      <!-- NAV LINKS -->
+      <!-- Options: (1. top-[-100vh] - this will make the nav links disappear from the viewport (negative height)  -->
+      <nav 
+      :class="isNavOpen ? 'top-0':'top-[-100vh]'" 
+      class="nav absolute px-4 dark:bg-dmBlack bg-white z-10 h-screen w-full left-0 flex flex-col justify-center gap-4 tracking-tighter
+      lg:top-0
+      lg:h-auto
+      lg:w-full
+      lg:relative
+      lg:flex
+      lg:flex-row
+      lg:justify-end
+      lg:gap-4
+      lg:px-0
+      ">
 
-    </div>
+        <div v-for="(link, index) in links" :key="index"  class=" flex justify-center">
+          <a @click="scrollTo(link.section)" :href="link.href" class="flex relative  gap-4 sm:justify-center lg:flex-none lg:gap-0
+            before:ease-out 
+            before:duration-300
+            before:content-['']
+            before:absolute
+            before:h-0.5
+            before:w-0
+            before:bg-gray-900
+            before:bottom-0
+            before:left-0
+            hover:before:w-full
+            dark:before:bg-dmWhite
+          ">
+            <span class="text-gray-500 dark:text-dmGray lg:text-[.5rem] lg:absolute lg:left-0 lg:top-0 lg:translate-y-[-50%]">/{{ '0'+(index + 1)  }}</span>
+            <div class="text-gray-900 dark:text-dmWhite text-5xl sm:text-[4rem] uppercase font-light lg:text-lg lg:font-medium ">{{ link.name }}</div>
+          </a>
+        </div>
 
-    <!-- NAV LINKS -->
-    <!-- Options: (1. top-[-100vh] - this will make the nav links disappear from the viewport (negative height)  -->
-    <nav 
-    :class="isNavOpen ? 'top-0':'top-[-100vh]'" 
-    class="nav absolute px-4 dark:bg-dmBlack bg-white z-10 h-screen w-full left-0 flex flex-col justify-center gap-4 tracking-tighter
-    lg:top-0
-    lg:h-auto
-    lg:w-full
-    lg:relative
-    lg:flex
-    lg:flex-row
-    lg:justify-end
-    lg:gap-4
-    lg:px-0
-    ">
-
-      <div v-for="(link, index) in links" :key="index"  class=" flex justify-center">
-        <a @click="scrollTo(link.section)" :href="link.href" class="flex relative  gap-4 sm:justify-center lg:flex-none lg:gap-0
-          before:ease-out 
-          before:duration-300
-          before:content-['']
-          before:absolute
-          before:h-0.5
-          before:w-0
-          before:bg-gray-900
-          before:bottom-0
-          before:left-0
-          hover:before:w-full
-          dark:before:bg-dmWhite
-        ">
-          <span class="text-gray-500 dark:text-dmGray lg:text-[.5rem] lg:absolute lg:left-0 lg:top-0 lg:translate-y-[-50%]">/{{ '0'+(index + 1)  }}</span>
-          <div class="text-gray-900 dark:text-dmWhite text-5xl sm:text-[4rem] uppercase font-light lg:text-lg lg:font-medium ">{{ link.name }}</div>
-        </a>
-      </div>
-
-      
-      <div class="pl-4 border-l border-slate-600 lg:block hidden">
         
-        <BaseToggleSwitch></BaseToggleSwitch>
+        <div class="pl-4 border-l border-slate-600 lg:block hidden">
+          
+          <BaseToggleSwitch></BaseToggleSwitch>
 
-      </div>
-      
+        </div>
+        
 
 
 
-    </nav>
-  </header>
+      </nav>
+    </header>
+  </div>
+
+
+  <!-- PRE-SPACER -->
+  <div ref="preSpacer" class="container h-[64px]  mx-auto py-2 px-4 xs:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-16 border-4 border-red-400"></div>  
+
+
+
 </template>
 <script>
 import BaseToggleSwitch from '../base/BaseToggleSwitch.vue';
 import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default {
   name: 'PauliversoncTheHeader',
@@ -149,6 +159,11 @@ export default {
         opacity: 0, // Example animation property
         delay: 1,
       });
+
+
+      const preSpacer = this.$refs.preSpacer;
+      console.log(preSpacer)
+
     },
   },
 }
